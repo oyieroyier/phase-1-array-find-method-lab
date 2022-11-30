a73fa6e686417a2bf8957cdd97fee5d625c31ad2
Bob Oyier <boboyier92@gmail.com>
2022-11-30 09:13:23 +0300

    "Completed the Array.prototype.find() assignment"

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const record = [
+	{ year: "2015", result: "W" },
+	{ year: "2014", result: "N/A" },
+	{ year: "2013", result: "L" }
+];
+
+function superbowlWin(someArray) {
+	for (const i of someArray) {
+		if (i.result === "W") {
+			return i.year;
+		}
+	}
+}
+console.log(record.find(superbowlWin))
